47df837a0a4643633ad330c81b56bc7d4dcea912
samuchapel <lemessamuel828@gmail.com>
2025-12-09 15:00:37 -0300

    vai ??

--- static/js/supabase.js
+++ static/js/supabase.js
@@ -1,8 +1,6 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-// CONFIGURAÇÕES DO SEU PROJETO
 const SUPABASE_URL = "https://rcwdvxbjqqtnkyriaciy.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjd2R2eGJqcXF0bmt5cmlhY2l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyOTc1ODUsImV4cCI6MjA4MDg3MzU4NX0.eg1zeu6M5-4AiS5LcwX6-4RUxuKi1xAGsMuTO7vzTjI";
 
-// Criar cliente
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
